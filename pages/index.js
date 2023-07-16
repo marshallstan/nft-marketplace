3b0b7be441924cfade576b911d2452d2debc5bc8
Marshall <narutowxl@gmail.com>
2023-07-16 22:12:26 +0800

    BigNFTSlider

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Style from '../styles/index.module.css'
-import { HeroSection, Service } from '../components/componentsindex'
+import { HeroSection, Service, BigNFTSlider } from '../components/componentsindex'
 
 const Home = () => {
   return (
     <div className={Style.homePage}>
       <HeroSection />
       <Service />
+      <BigNFTSlider />
     </div>
   )
 }
