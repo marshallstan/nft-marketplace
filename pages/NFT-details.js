86260a28c5c2a84b2477a252d0c6f09c63809346
Marshall <narutowxl@gmail.com>
2023-07-25 00:12:55 +0800

    NFTDetails page

---
+++ pages/NFT-details.js
@@ -0,0 +1,14 @@
+import { Category, Brand } from '../components/componentsindex'
+import NFTDetailsPage from '../NFTDetailsPage/NFTDetailsPage'
+
+const NFTDetails = () => {
+  return (
+    <div>
+      <NFTDetailsPage />
+      <Category />
+      <Brand />
+    </div>
+  )
+}
+
+export default NFTDetails
