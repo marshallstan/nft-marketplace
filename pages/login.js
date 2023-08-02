9977e98d1e2617c8133fcd8e179d4927efd7f16d
Marshall <narutowxl@gmail.com>
2023-08-02 16:13:13 +0800

    login page

---
+++ pages/login.js
@@ -0,0 +1,18 @@
+import Style from '../styles/login.module.css'
+import LoginAndSignUp from '../loginAndSignUp/loginAndSignUp'
+
+const login = () => {
+  return (
+    <div className={Style.login}>
+      <div className={Style.login_box}>
+        <h1>Login</h1>
+        <LoginAndSignUp />
+        <p className={Style.login_box_para}>
+          New user? <a href="#">Create an account</a>
+        </p>
+      </div>
+    </div>
+  )
+}
+
+export default login
